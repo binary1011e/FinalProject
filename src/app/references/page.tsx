0a2cdd4474ac92742ef1b3d5b1d8a6dc707723b6
binary1011e <aidan_c_wang@brown.edu>
2025-05-05 21:13:12 -0400

    DONE???

--- src/app/references/page.tsx
+++ src/app/references/page.tsx
@@ -9,6 +9,9 @@ export default function ReferencesPage() {
         <li>Duarte & Mayer (2025)</li>
         <li>Cochrane (2024)</li>
         <li>Reddit Data - Data prior to April 2023 was collected by Pushshift, data after that was collected by u/raiderbdev here. Extracted, split and re-packaged by u/Watchful1. And hosted on academictorrents.com.</li>
+        <li>Twitter-Roberta model - https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment-latest</li>
+        <li>Link to github website repo: https://github.com/binary1011e/FinalProject</li>
+        <li>Link to github python repo: https://github.com/binary1011e/pythonEcon</li>
       </ul>
     </section>
   )
